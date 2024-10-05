7e4639cc255e88b0e5fb5b3480125125e8f82ea1
insun0816 <jsa9292@gmail.com>
2024-10-06 06:22:18 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -346,6 +346,15 @@ function startListening() {
         }
         //transcription
         outlineHelper(transcription_current_under, inputText);
+        if (isFinal) {
+            var newLine = transcription_current.cloneNode(true);
+            transcription.insertBefore(newLine, transcription_current);
+            newLine.classList.add("fade-out-5s");
+            newLine.onanimationend = () => {
+                newLine.remove();
+            };
+            outlineHelper(transcription_current_under, " ");
+        }
         if (translateCycle < translateThres && !isFinal) {
             translateCycle += 1;
             return;
@@ -364,6 +373,17 @@ function startListening() {
                     json[0].map((item) => item[0]).join("")
                 );
             })
+            .then(() => {
+                if (isFinal) {
+                    var newLine1 = translation1_current.cloneNode(true);
+                    translation1.insertBefore(newLine1, translation1_current);
+                    newLine1.classList.add("fade-out-5s");
+                    newLine1.onanimationend = () => {
+                        newLine1.remove();
+                    };
+                    outlineHelper(translation1_current_under, " ");
+                }
+            })
             .catch((error) => {
                 console.log(error);
             });
@@ -379,35 +399,20 @@ function startListening() {
                     json[0].map((item) => item[0]).join("")
                 );
             })
+            .then(() => {
+                if (isFinal) {
+                    var newLine2 = translation2_current.cloneNode(true);
+                    translation2.insertBefore(newLine2, translation2_current);
+                    newLine2.classList.add("fade-out-5s");
+                    newLine2.onanimationend = () => {
+                        newLine2.remove();
+                    };
+                    outlineHelper(translation2_current_under, " ");
+                }
+            })
             .catch((error) => {
                 console.log(error);
             });
-
-        if (isFinal) {
-            var newLine = transcription_current.cloneNode(true);
-            transcription.insertBefore(newLine, transcription_current);
-            newLine.classList.add("fade-out-5s");
-            newLine.onanimationend = () => {
-                newLine.remove();
-            };
-            outlineHelper(transcription_current_under, " ");
-
-            var newLine1 = translation1_current.cloneNode(true);
-            translation1.insertBefore(newLine1, translation1_current);
-            newLine1.classList.add("fade-out-5s");
-            newLine1.onanimationend = () => {
-                newLine1.remove();
-            };
-            outlineHelper(translation1_current_under, " ");
-
-            var newLine2 = translation2_current.cloneNode(true);
-            translation2.insertBefore(newLine2, translation2_current);
-            newLine2.classList.add("fade-out-5s");
-            newLine2.onanimationend = () => {
-                newLine2.remove();
-            };
-            outlineHelper(translation2_current_under, " ");
-        }
     };
     recognition.onerror = function (event) {
         startBtn.disabled = false;
